ab0ecb1b3e41d3210a8ad9471116fbd779ca1c8b
Nikolay Vigdorov <vignikbor@gmail.com>
2021-06-13 14:58:46 +0300

    test deploy

--- src/pages/main/components/page/Page.tsx
+++ src/pages/main/components/page/Page.tsx
@@ -3,7 +3,7 @@ import React, {memo} from 'react';
 const MainPage: React.FC = () => {
     return (
         <div>
-            <div>Админка Crypto-bot</div>
+            <div>Админка Crypto-bot v1</div>
 
             <div>Выбирите нужный раздел</div>
         </div>
